5c9a4772ba98f65d37d89561e5c8d40b84bc3963
visualstudio <visualstudio@122-PC34>
2019-03-13 09:21:39 -0400

    9:21am

---
+++ bank/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    public class Class1
+    {
+    }
+}
